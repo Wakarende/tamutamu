5b6c902ff8867a76a8a423197af736dfcdf520cc
Joy Kirii <joykirii@gmail.com>
2021-03-06 22:33:50 +0300

    style contact section

--- js/script.js
+++ js/script.js
@@ -1,4 +1,25 @@
+// menu 
+$(document).ready(function() {
+  $("#click").on(click, function(event) {
+    event.preventDefault();
+    $(".card").slideDown();
+  });
+});
+
+
+
+
 // business interface
+function Pizza(pizza, size, crust, toppings) {
+  this.pizza = pizza;
+  this.size = size;
+  this.crust = crust;
+  this.toppings = toppings;
+}
+
+var sizePrice, crustPrice, 
+
+
 $(document).ready(function() {
   $("#submit").click(function(event) {
     event.preventDefault();
