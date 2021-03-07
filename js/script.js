c20ff62403489aeaf8362bd2cafaaaee4ef71fc6
Joy Kirii <joykirii@gmail.com>
2021-03-07 19:51:08 +0300

    add constructors ,prototypes to calculate pizza total. add function to add delivery cost. add alerts for total cost

--- js/script.js
+++ js/script.js
@@ -1,36 +1,91 @@
-// menu 
-$(document).ready(function() {
-  $("#click").on(click, function(event) {
-    event.preventDefault();
-    $(".card").hide();
-  });
-});
-
-
-
-
-// business interface
-function Pizza(pizza, size, crust, toppings) {
-  this.pizza = pizza;
-  this.size = size;
-  this.crust = crust;
-  this.toppings = toppings;
+//business interface
+function Pizza(toppings,size,crust) {
+this.toppings = toppings;
+this.size = size;
+this.crust = crust;
 }
 
-var sizePrice, crustPrice, 
+Pizza.prototype.totalToppings = function() {
+  if(this.toppings.length != 0) {
+    var toppingPrice = this.toppings.length * 100;
+    return toppingPrice;
+  }else
+  alert("kindly choose your toppings!")
+}
 
+Pizza.prototype.pizzaSize = function () {
+  if(this.size === "Small"){
+    return 600;
+  }else if(this.size === "Medium"){
+    return 800;
+  }else if (this.size === "Large") {
+    return 1000;
+  }else
+  alert("kindly select a pizza size!");
+}
 
+Pizza.prototype.crustType = function () {
+  if(this.crust === "Thick") {
+    return 150
+  } else if (this.crust === "Normal") {
+    return 100
+  }else if (this.crust === "Thin") {
+    return 50
+  }else
+  alert("kindly choose a crust size.")
+  }
+
+let finalCost;
+Pizza.prototype.totalCost = function(totalToppings, pizzaSize, crustType) {
+   finalCost = totalToppings + pizzaSize + crustType;
+  $("#displayTotalCost").html(finalCost);
+  ;
+}
+
+//user interface
+
+//calculation of total pizza price
+var pizza, pizzaQuantity;
 $(document).ready(function() {
-  $("#submit").click(function(event) {
+  $("#form").submit(function(event){
     event.preventDefault();
-    let pizzaType = $("#pizza option:selected").val();
-    let sizeType = $( "#size option:selected").val();
-    let crustType = $( "#crust option:selected").val();
-    let pizzaTopping = []
-      $("input:checkbox[name=toppings]:checked").each(function() {
-        pizzaTopping.push($(this).val());
-      });
-  });
+    $("#show-Bill").show();
+    var checkedSize = $("input:radio[name=sizePizza]:checked").val();
+    var checkedCrust = $("input:radio[name=crustPizza]:checked").val();
+    var checkedToppings = [];
+    $("input:checkbox[name=toppings]:checked").each(function() {
+      checkedToppings.push($(this).val());
+    });
+    pizzaQuantity = parseInt($("#quantity").val());
 
-  var totalCost = 
+    $("#displaySize").text(checkedSize);
+    $("#displayCrust").text(checkedCrust);
+
+
+    pizza = new Pizza(checkedToppings,checkedSize,checkedCrust);
+
+    var totalToppings = pizza.totalToppings();
+    var pizzaSize = pizza.pizzaSize();
+    var crustType = pizza.crustType();
+    pizza.totalCost(pizzaSize,totalToppings,crustType);
+    console.log(pizza);
+  });
 });
+
+//pickup option
+$("#pickup").click(function() {
+  alert("Your order will be ready within the hour. Your order total is " + finalCost);
+});
+
+//delivery option
+$("#submit").click(function() {
+  var fullName = $("#name").val();
+  var location= $("#location").val();
+  var phoneNumber = $("#number").val();
+  var finalDeliveryTotal = finalCost +150;
+  if(fullName == "" || location == "" || phoneNumber == "") {
+    alert("please enter your details")
+  }else {
+    alert("Dear " + fullName + " your pizza will be delivered to " + location + " within the hour." + "Your total cost is " + finalDeliveryTotal);
+  }
+})
